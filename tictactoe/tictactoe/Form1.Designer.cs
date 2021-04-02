3e05a77bd5b386d046d6550cd1af8210ed47adbc
Aliaksandr Aleksiayuk <48724263+kollerkap@users.noreply.github.com>
2021-04-02 13:27:51 +0200

    Add files via upload

---
+++ tictactoe/tictactoe/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace tictactoe
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitAltF4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.computerModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.userVsUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.T1 = new System.Windows.Forms.Button();
+			this.T2 = new System.Windows.Forms.Button();
+			this.T3 = new System.Windows.Forms.Button();
+			this.M1 = new System.Windows.Forms.Button();
+			this.M2 = new System.Windows.Forms.Button();
+			this.M3 = new System.Windows.Forms.Button();
+			this.L1 = new System.Windows.Forms.Button();
+			this.L2 = new System.Windows.Forms.Button();
+			this.L3 = new System.Windows.Forms.Button();
+			this.userVsPCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.computerModeToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(22, 7, 0, 7);
+			this.menuStrip1.Size = new System.Drawing.Size(329, 33);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// menuToolStripMenuItem
+			// 
+			this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitAltF4ToolStripMenuItem});
+			this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+			this.menuToolStripMenuItem.Size = new System.Drawing.Size(51, 19);
+			this.menuToolStripMenuItem.Text = "Menu";
+			// 
+			// newGameToolStripMenuItem
+			// 
+			this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+			this.newGameToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+			this.newGameToolStripMenuItem.Text = "New Game";
+			this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+			// 
+			// exitAltF4ToolStripMenuItem
+			// 
+			this.exitAltF4ToolStripMenuItem.Name = "exitAltF4ToolStripMenuItem";
+			this.exitAltF4ToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+			this.exitAltF4ToolStripMenuItem.Text = "Exit (Alt+F4)";
+			this.exitAltF4ToolStripMenuItem.Click += new System.EventHandler(this.exitAltF4ToolStripMenuItem_Click);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(45, 19);
+			this.helpToolStripMenuItem.Text = "Help";
+			this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(53, 19);
+			this.aboutToolStripMenuItem.Text = "About";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+			// 
+			// computerModeToolStripMenuItem
+			// 
+			this.computerModeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userVsUserToolStripMenuItem,
+            this.userVsPCToolStripMenuItem});
+			this.computerModeToolStripMenuItem.Name = "computerModeToolStripMenuItem";
+			this.computerModeToolStripMenuItem.Size = new System.Drawing.Size(51, 19);
+			this.computerModeToolStripMenuItem.Text = "Mode";
+			// 
+			// userVsUserToolStripMenuItem
+			// 
+			this.userVsUserToolStripMenuItem.Name = "userVsUserToolStripMenuItem";
+			this.userVsUserToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.userVsUserToolStripMenuItem.Text = "User vs User ";
+			this.userVsUserToolStripMenuItem.Click += new System.EventHandler(this.mode_user_vs_user);
+			// 
+			// T1
+			// 
+			this.T1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.T1.Location = new System.Drawing.Point(28, 80);
+			this.T1.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.T1.Name = "T1";
+			this.T1.Size = new System.Drawing.Size(75, 75);
+			this.T1.TabIndex = 1;
+			this.T1.UseVisualStyleBackColor = true;
+			this.T1.Click += new System.EventHandler(this.button_click);
+			this.T1.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.T1.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// T2
+			// 
+			this.T2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.T2.Location = new System.Drawing.Point(125, 80);
+			this.T2.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.T2.Name = "T2";
+			this.T2.Size = new System.Drawing.Size(75, 75);
+			this.T2.TabIndex = 2;
+			this.T2.UseVisualStyleBackColor = true;
+			this.T2.Click += new System.EventHandler(this.button_click);
+			this.T2.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.T2.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// T3
+			// 
+			this.T3.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.T3.Location = new System.Drawing.Point(222, 80);
+			this.T3.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.T3.Name = "T3";
+			this.T3.Size = new System.Drawing.Size(75, 75);
+			this.T3.TabIndex = 3;
+			this.T3.UseVisualStyleBackColor = true;
+			this.T3.Click += new System.EventHandler(this.button_click);
+			this.T3.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.T3.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// M1
+			// 
+			this.M1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.M1.Location = new System.Drawing.Point(28, 173);
+			this.M1.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.M1.Name = "M1";
+			this.M1.Size = new System.Drawing.Size(75, 75);
+			this.M1.TabIndex = 4;
+			this.M1.UseVisualStyleBackColor = true;
+			this.M1.Click += new System.EventHandler(this.button_click);
+			this.M1.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.M1.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// M2
+			// 
+			this.M2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.M2.Location = new System.Drawing.Point(125, 173);
+			this.M2.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.M2.Name = "M2";
+			this.M2.Size = new System.Drawing.Size(75, 75);
+			this.M2.TabIndex = 5;
+			this.M2.UseVisualStyleBackColor = true;
+			this.M2.Click += new System.EventHandler(this.button_click);
+			this.M2.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.M2.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// M3
+			// 
+			this.M3.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.M3.Location = new System.Drawing.Point(222, 173);
+			this.M3.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.M3.Name = "M3";
+			this.M3.Size = new System.Drawing.Size(75, 75);
+			this.M3.TabIndex = 6;
+			this.M3.UseVisualStyleBackColor = true;
+			this.M3.Click += new System.EventHandler(this.button_click);
+			this.M3.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.M3.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// L1
+			// 
+			this.L1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.L1.Location = new System.Drawing.Point(28, 260);
+			this.L1.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.L1.Name = "L1";
+			this.L1.Size = new System.Drawing.Size(75, 75);
+			this.L1.TabIndex = 7;
+			this.L1.UseVisualStyleBackColor = true;
+			this.L1.Click += new System.EventHandler(this.button_click);
+			this.L1.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.L1.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// L2
+			// 
+			this.L2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.L2.Location = new System.Drawing.Point(125, 260);
+			this.L2.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.L2.Name = "L2";
+			this.L2.Size = new System.Drawing.Size(75, 75);
+			this.L2.TabIndex = 8;
+			this.L2.UseVisualStyleBackColor = true;
+			this.L2.Click += new System.EventHandler(this.button_click);
+			this.L2.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.L2.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// L3
+			// 
+			this.L3.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.L3.Location = new System.Drawing.Point(222, 260);
+			this.L3.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.L3.Name = "L3";
+			this.L3.Size = new System.Drawing.Size(75, 75);
+			this.L3.TabIndex = 9;
+			this.L3.UseVisualStyleBackColor = true;
+			this.L3.Click += new System.EventHandler(this.button_click);
+			this.L3.MouseEnter += new System.EventHandler(this.mouse_enter);
+			this.L3.MouseLeave += new System.EventHandler(this.mouse_leave);
+			// 
+			// userVsPCToolStripMenuItem
+			// 
+			this.userVsPCToolStripMenuItem.Name = "userVsPCToolStripMenuItem";
+			this.userVsPCToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.userVsPCToolStripMenuItem.Text = "User vs PC";
+			this.userVsPCToolStripMenuItem.Click += new System.EventHandler(this.mode_user_vs_pc);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(329, 362);
+			this.Controls.Add(this.L3);
+			this.Controls.Add(this.L2);
+			this.Controls.Add(this.L1);
+			this.Controls.Add(this.M3);
+			this.Controls.Add(this.M2);
+			this.Controls.Add(this.M1);
+			this.Controls.Add(this.T3);
+			this.Controls.Add(this.T2);
+			this.Controls.Add(this.T1);
+			this.Controls.Add(this.menuStrip1);
+			this.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Tic Tac Toe by AniMount";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitAltF4ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.Button T1;
+		private System.Windows.Forms.Button T2;
+		private System.Windows.Forms.Button T3;
+		private System.Windows.Forms.Button M1;
+		private System.Windows.Forms.Button M2;
+		private System.Windows.Forms.Button M3;
+		private System.Windows.Forms.Button L1;
+		private System.Windows.Forms.Button L2;
+		private System.Windows.Forms.Button L3;
+		private System.Windows.Forms.ToolStripMenuItem computerModeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem userVsUserToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem userVsPCToolStripMenuItem;
+	}
+}
+
---
+++ tictactoe/tictactoe/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+	public partial class Form1 : Form
+	{
+		bool uservspc = false;
+		bool turn = true;
+		int turn_count = 0;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		/* Watermark of a bad student
+		private void button1_Click(object sender, EventArgs e){}
+		private void B1_Click(object sender, EventArgs e){}
+		*/
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Created by Alex\nVerson alfa 2.2.1\nWhat's new:\n 1. Added new mode\n 2. Autostart every end of game\n 3. Minor fixes\nThanks for using AniMount products\n Alex<3", "About");
+		}
+
+		private void exitAltF4ToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Welcome to Tic Tac Toe by Alex\n\n\nRULES FOR TIC-TAC-TOE\n1.The game is played on a grid that's 3 squares by 3 squares.\n2.You are X, your friend(or the computer in this case) is O.Players take turns putting their marks in empty squares.\n3.The first player to get 3 of her marks in a row(up, down, across, or diagonally) is the winner.\n4.When all 9 squares are full, the game is over.If no player has 3 marks in a row, the game ends in a tie.\nCan you win? Yes, but you can try your best\n Good luck!", "Help");
+		}
+
+		private void button_click(object sender, EventArgs e)
+		{
+			Button b = (Button)sender;
+			if (turn)
+				b.Text = "x";
+			else
+				b.Text = "o";
+
+			turn = !turn;
+			b.Enabled = false;
+
+			turn_count++;
+
+			winnercheck();
+
+			if ((!turn) && (uservspc))
+			{
+				pc_choice();
+			}//logic of our pc making choises playing against us
+
+		}//buttonclick
+
+		/// <logic user vs pc mode>
+		
+		private void pc_choice()
+		{
+			
+				//priority 1:  get tick tac toe
+				//priority 2:  block x tic tac toe
+				//priority 3:  go for corner space
+				//priority 4:  pick open space
+
+				Button move = null;
+
+				//look for tic tac toe opportunities
+				move = look_for_win_or_block("o"); //look for win
+				if (move == null)
+				{
+					move = look_for_win_or_block("x"); //look for block
+					if (move == null)
+					{
+						move = look_for_corner();
+						if (move == null)
+						{
+							move = look_for_open_space();
+						}//end if
+					}//end if
+				}//end if
+
+				move.PerformClick();
+			}
+
+			private Button look_for_open_space()
+			{
+				Console.WriteLine("Looking for open space");
+				Button b = null;
+				foreach (Control c in Controls)
+				{
+					b = c as Button;
+					if (b != null)
+					{
+						if (b.Text == "")
+							return b;
+					}//end if
+				}//end if
+
+				return null;
+			}
+
+			private Button look_for_corner()
+			{
+				Console.WriteLine("Looking for corner");
+				if (T1.Text == "o")
+				{
+					if (T3.Text == "")
+						return T3;
+					if (L3.Text == "")
+						return L3;
+					if (L1.Text == "")
+						return L1;
+				}
+
+				if (T3.Text == "o")
+				{
+					if (T1.Text == "")
+						return T1;
+					if (L3.Text == "")
+						return L3;
+					if (L1.Text == "")
+						return L1;
+				}
+
+				if (L3.Text == "o")
+				{
+					if (T1.Text == "")
+						return T3;
+					if (T3.Text == "")
+						return T3;
+					if (L1.Text == "")
+						return L1;
+				}
+
+				if (L1.Text == "o")
+				{
+					if (T1.Text == "")
+						return T3;
+					if (T3.Text == "")
+						return T3;
+					if (L3.Text == "")
+						return L3;
+				}
+
+				if (T1.Text == "")
+					return T1;
+				if (T3.Text == "")
+					return T3;
+				if (L1.Text == "")
+					return L1;
+				if (L3.Text == "")
+					return L3;
+
+				return null;
+			}
+
+			private Button look_for_win_or_block(string mark)
+			{
+				Console.WriteLine("Looking for win or block:  " + mark);
+				//HORIZONTAL TESTS
+				if ((T1.Text == mark) && (T2.Text == mark) && (T3.Text == ""))
+					return T3;
+				if ((T2.Text == mark) && (T3.Text == mark) && (T1.Text == ""))
+					return T1;
+				if ((T1.Text == mark) && (T3.Text == mark) && (T2.Text == ""))
+					return T2;
+
+				if ((M1.Text == mark) && (M2.Text == mark) && (M3.Text == ""))
+					return M3;
+				if ((M2.Text == mark) && (M3.Text == mark) && (M1.Text == ""))
+					return M1;
+				if ((M1.Text == mark) && (M3.Text == mark) && (M2.Text == ""))
+					return M2;
+
+				if ((L1.Text == mark) && (L2.Text == mark) && (L3.Text == ""))
+					return L3;
+				if ((L2.Text == mark) && (L3.Text == mark) && (L1.Text == ""))
+					return L1;
+				if ((L1.Text == mark) && (L3.Text == mark) && (L2.Text == ""))
+					return L2;
+
+				//VERTICAL TESTS
+				if ((T1.Text == mark) && (M1.Text == mark) && (L1.Text == ""))
+					return L1;
+				if ((M1.Text == mark) && (L1.Text == mark) && (T1.Text == ""))
+					return T1;
+				if ((T1.Text == mark) && (L1.Text == mark) && (M1.Text == ""))
+					return M1;
+
+				if ((T2.Text == mark) && (M2.Text == mark) && (L2.Text == ""))
+					return L2;
+				if ((M2.Text == mark) && (L2.Text == mark) && (T2.Text == ""))
+					return T2;
+				if ((T2.Text == mark) && (L2.Text == mark) && (M2.Text == ""))
+					return M2;
+
+				if ((T3.Text == mark) && (M3.Text == mark) && (L3.Text == ""))
+					return L3;
+				if ((M3.Text == mark) && (L3.Text == mark) && (T3.Text == ""))
+					return T3;
+				if ((T3.Text == mark) && (L3.Text == mark) && (M3.Text == ""))
+					return M3;
+
+				//DIAGONAL TESTS
+				if ((T1.Text == mark) && (M2.Text == mark) && (L3.Text == ""))
+					return L3;
+				if ((M2.Text == mark) && (L3.Text == mark) && (T1.Text == ""))
+					return T1;
+				if ((T1.Text == mark) && (L3.Text == mark) && (M2.Text == ""))
+					return M2;
+
+				if ((T3.Text == mark) && (M2.Text == mark) && (L1.Text == ""))
+					return L1;
+				if ((M2.Text == mark) && (L1.Text == mark) && (T3.Text == ""))
+					return T3;
+				if ((T3.Text == mark) && (L1.Text == mark) && (M2.Text == ""))
+					return M2;
+
+				return null;
+			
+		}
+
+		/// </logic user vs pc mode>
+
+		private void winnercheck ()
+		{
+			bool player_win = false;
+
+			// horizontal
+			if ((T1.Text == T2.Text) && (T2.Text == T3.Text) && (!T1.Enabled))
+				player_win = true;
+			else if ((M1.Text == M2.Text) && (M2.Text == M3.Text) && (!M1.Enabled))
+				player_win = true;
+			else if ((L1.Text == L2.Text) && (L2.Text == L3.Text) && (!L1.Enabled))
+				player_win = true;
+			// vertical
+			else if ((T1.Text == M1.Text) && (M1.Text == L1.Text) && (!T1.Enabled))
+				player_win = true;
+			else if ((T2.Text == M2.Text) && (M2.Text == L2.Text) && (!M2.Enabled))
+				player_win = true;
+			else if ((T3.Text == M3.Text) && (M3.Text == L3.Text) && (!L3.Enabled))
+				player_win = true;
+			// cross
+			else if ((T1.Text == M2.Text) && (M2.Text == L3.Text) && (!T1.Enabled))
+				player_win = true;
+			else if ((L1.Text == M2.Text) && (M2.Text == T3.Text) && (!L1.Enabled))
+				player_win = true;
+			if (player_win)
+			{
+				disablebuttons();
+
+				String winner = "";
+				if (turn)
+					winner = "o";
+				else
+					winner = "x";
+
+				MessageBox.Show(winner + " Wins!", "Congratulations!");
+				Autostart();
+			}//ifwinner
+			else if (turn_count == 9)
+			{
+				MessageBox.Show("Draw","Unfortunatly");
+				Autostart();
+			}
+		}//winnercheck
+		private void disablebuttons()
+		{
+			try
+			{
+				foreach (Control c in Controls)
+				{
+					Button b = (Button)c;
+					b.Enabled = false;
+				}//foreach
+			}
+			catch { }
+		}//disablebuttons
+
+		private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			turn = true;
+			turn_count = 0;
+			try
+			{
+				foreach (Control c in Controls)
+				{
+					Button b = (Button)c;
+					b.Enabled = true;
+					b.Text = "";
+				}//foreach2
+			}
+			catch { }
+		}
+
+		private void mouse_enter(object sender, EventArgs e)
+		{
+			Button b = (Button)sender;
+			if (b.Enabled)
+			{
+				if (turn)
+					b.Text = "x";
+				else
+					b.Text = "o";
+			}//enable button
+		}
+
+		private void mouse_leave(object sender, EventArgs e)
+		{
+			Button b = (Button)sender;
+			if (b.Enabled)
+			{
+				b.Text = "";
+			}//enable button2
+		}
+
+		private void Autostart()// Add after every messagebox Win/Draw.
+		{
+			disablebuttons(); 
+			turn = true;
+			turn_count = 0;
+			try
+			{
+				foreach (Control c in Controls)
+				{
+					Button b = (Button)c;
+					b.Enabled = true;
+					b.Text = "";
+				}//foreach2
+			}
+			catch { }
+		}//Autostart
+
+
+		private void mode_user_vs_pc(object sender, EventArgs e)
+		{
+			Autostart();
+			uservspc = true;
+			MessageBox.Show("Before you start\nHere's some information you used know\n 1. You start first\n 2. You can win\n 3. Enjoy the game!","Welcome to User vs Pc mode");
+			
+		}//user vs pc
+
+		private void mode_user_vs_user(object sender, EventArgs e)
+		{
+			Autostart();
+			uservspc = false;
+			MessageBox.Show("Old sweet user vs user.\n Enjoy the game!", "Welcome to User vs User mode");
+		}//user vs user 
+
+		
+	}//Form1
+}//tictactoe
